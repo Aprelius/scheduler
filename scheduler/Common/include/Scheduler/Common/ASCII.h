4c445f07870f9d2c042faa12b78e313201388944
Daniel Weiner <danny@phantomnet.net>
2018-09-22 17:24:43 -0700

    Added ASCII helpers

---
+++ scheduler/Common/include/Scheduler/Common/ASCII.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace Scheduler {
+
+    static const char* hexChars = "0123456789abcdef";
+
+    inline bool IsDigit(char c) { return c >= 48 && c <= 57; }
+
+    inline bool IsSpace(char c) { return (c == ' ' || c == '\t' || c == '\n' || c == '\r'); }
+
+    inline bool IsLower(char c) { return c >= 97 && c <= 122; }
+
+	inline bool IsUpper(char c) { return c >= 65 && c <= 90; }
+
+    inline static unsigned char ToNibble(char c)
+	{
+		if (IsDigit(c)) return c - '0';
+		if (IsLower(c)) return c - 'a' + 10;
+		if (IsUpper(c)) return c - 'A' + 10;
+		return 255;
+	}
+
+}  // namespace Scheduler
