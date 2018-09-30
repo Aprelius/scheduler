17474cf6a52a54f013bcc4e0edfcb3aadfaa92e1
Daniel Weiner <danny@phantomnet.net>
2018-09-29 21:43:46 -0700

    The Big Kahuna: Added the first Scheduling implementation

---
+++ scheduler/Lib/include/Scheduler/Lib/Scheduler.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <Scheduler/Common/Error.h>
+#include <Scheduler/Lib/Chain.h>
+#include <Scheduler/Lib/Executor.h>
+#include <Scheduler/Lib/Task.h>
+#include <Scheduler/Lib/TaskManager.h>
+#include <Scheduler/Lib/UUID.h>
+#include <condition_variable>
+#include <deque>
+#include <memory>
+#include <mutex>
+#include <set>
+#include <thread>
+#include <unordered_map>
+
+namespace Scheduler {
+namespace Lib {
+
+    class ScheduleReporter;
+
+    struct SchedulerParams
+    {
+        /// Param block for the Task executor. It is only needed if a
+        /// pointer to an already existing executor is not supplied.
+        ExecutorParams executorParams;
+
+        /// Pointer to a supplied Task executor which will handle
+        /// executing the individual tasks onces scheduled.
+        Executor* executor = nullptr;
+
+        /// Pointer to the Reporter which should be used for notifying
+        /// listeners that an event happened.
+        ScheduleReporter* reporter = nullptr;
+
+        /// Param block for the Task manager. It is only needed if a
+        /// pointer to an already existing manager is not supplied.
+        TaskManagerParams managerParams;
+
+        /// Pointer to the TaskManager object which will serve the
+        /// scheduler with Metadata about Tasks.
+        TaskManager* manager = nullptr;
+    };
+
+    class ScheduleReporter :
+        public std::enable_shared_from_this<ScheduleReporter>
+    {
+    public:
+        virtual void Shutdown(bool wait = true);
+    };
+
+    class TaskScheduler;
+    typedef std::shared_ptr<TaskScheduler> SchedulerPtr;
+    typedef std::shared_ptr<ScheduleReporter> ScheduleReporterPtr;
+
+    class TaskScheduler : public std::enable_shared_from_this<TaskScheduler>
+    {
+        friend TaskRunner;
+
+    public:
+        static Error Create(const SchedulerParams& params, SchedulerPtr& sch);
+
+        virtual ~TaskScheduler();
+
+        void Enqueue(ChainPtr& chain);
+
+        void Enqueue(ChainPtr&& chain);
+
+        void Enqueue(TaskPtr& task);
+
+        void Enqueue(TaskPtr&& task);
+
+        void Notify();
+
+        void Run() { while (RunOnce()); }
+        bool RunOnce();
+
+        void Shutdown(bool wait = true);
+
+        void Start();
+
+    protected:
+        TaskScheduler(
+            const SchedulerParams& params,
+            std::shared_ptr<ScheduleReporter>&& reporter,
+            std::shared_ptr<TaskManager>&& manager,
+            std::shared_ptr<Executor>&& executor);
+
+        virtual Error Initialize();
+
+        void NotifyLocked(std::unique_lock<std::mutex>& lock);
+
+        void Notify(const UUID& id, TaskState state);
+
+    private:
+        void EnqueueLocked(TaskPtr&& task, std::unique_lock<std::mutex>& lock);
+        void EnqueueLocked(TaskPtr& task, std::unique_lock<std::mutex>& lock);
+
+        bool HandleTask(TaskPtr& task);
+        bool HandleExpiredTask(TaskPtr& task);
+
+        bool IsTimedOut(const TaskPtr& task) const;
+
+        bool ProcessActiveTasks();
+        bool ProcessPendingQueue();
+        bool ProcessPendingTasks();
+
+        void PrunePrematureTasks();
+
+        // Scheduler is wiating for changes
+        bool m_waiting = false;
+        // Scheduler has been requested to shutdown
+        bool m_shutdown = false;
+        // Scheduler is shutdown
+        bool m_shutdownComplete = false;
+
+        std::condition_variable m_cond;
+        std::mutex m_mutex;
+        std::deque<UUID> m_queue;
+        std::thread m_thread;
+
+        // Cache the UUID of tasks which are active on the executor
+        std::set<UUID> m_active;
+        // Cache the list of tasks which are currently known and pending
+        std::set<UUID> m_pending;
+        // Cache the list of tasks which are premature and cannot be
+        // run until a certain time.
+        std::unordered_map<UUID, Clock::time_point> m_premature;
+        // Cache the list of tasks which may be timed out due to an
+        // unqueued dependency.
+        std::unordered_map<UUID, Clock::time_point> m_timeouts;
+
+        std::shared_ptr<Executor> m_executor;
+        std::shared_ptr<ScheduleReporter> m_reporter;
+        std::shared_ptr<TaskManager> m_manager;
+    };
+
+}  // namespace Lib
+}  // namespace Scheduler
---
+++ tests/src/TestScheduling.cpp
@@ -0,0 +1,113 @@
+#include <gtest/gtest.h>
+
+#include <Scheduler/Common/Console.h>
+#include <Scheduler/Lib/Chain.h>
+#include <Scheduler/Lib/Scheduler.h>
+#include <Scheduler/Lib/Task.h>
+
+using namespace Scheduler;
+using namespace Scheduler::Lib;
+
+TEST(SchedularStartup, InitializeAndShutdown)
+{
+    SchedulerParams params;
+    SchedulerPtr scheduler;
+    ASSERT_EQ(TaskScheduler::Create(params, scheduler), E_SUCCESS);
+    scheduler->Start();
+
+    TaskPtr taskA = MakeTask<Task>(),
+            taskB = MakeTask<Task>();
+
+    scheduler->Enqueue(taskA);
+    scheduler->Enqueue(taskB);
+
+    taskB->Wait();
+    taskA->Wait();
+
+    ASSERT_EQ(taskA->GetState(), TaskState::SUCCESS);
+    ASSERT_EQ(taskB->GetState(), TaskState::SUCCESS);
+
+    scheduler->Shutdown(true);
+    ASSERT_FALSE(scheduler->RunOnce());
+}
+
+TEST(Scheduler, DependentTasks)
+{
+    SchedulerParams params;
+    SchedulerPtr scheduler;
+    ASSERT_EQ(TaskScheduler::Create(params, scheduler), E_SUCCESS);
+    scheduler->Start();
+
+    TaskPtr taskA = MakeTask<Task>(),
+            taskB = MakeTask<Task>();
+
+    Console(std::cout) << "taskA = " << taskA->ToString(true) << '\n';
+    Console(std::cout) << "taskB = " << taskB->ToString(true) << '\n';
+
+    taskA->Depends(taskB);
+    ASSERT_TRUE(taskA->IsValid());
+    ASSERT_TRUE(taskA->Requires(taskB));
+
+    scheduler->Enqueue(taskA);
+    scheduler->Enqueue(taskB);
+
+    taskB->Wait();
+    taskA->Wait();
+
+    ASSERT_EQ(taskA->GetState(), TaskState::SUCCESS);
+    ASSERT_EQ(taskB->GetState(), TaskState::SUCCESS);
+
+    scheduler->Shutdown(true);
+    ASSERT_FALSE(scheduler->RunOnce());
+}
+
+TEST(Scheduler, ProcessChainAndDependents)
+{
+    SchedulerParams params;
+    SchedulerPtr scheduler;
+    ASSERT_EQ(TaskScheduler::Create(params, scheduler), E_SUCCESS);
+    scheduler->Start();
+
+    TaskPtr taskA = MakeTask<Task>(),
+            taskB = MakeTask<Task>(),
+            taskC = MakeTask<Task>(),
+            taskD = MakeTask<Task>();
+
+    ChainPtr chain = MakeTask<Chain>(taskA, taskB, taskC);
+
+    Console(std::cout) << "taskA = " << taskA->ToString(true) << '\n';
+    Console(std::cout) << "taskB = " << taskB->ToString(true) << '\n';
+    Console(std::cout) << "taskC = " << taskC->ToString(true) << '\n';
+    Console(std::cout) << "taskD = " << taskD->ToString(true) << '\n';
+
+    taskD->Depends(chain.get());
+    ASSERT_TRUE(taskA->IsValid());
+    ASSERT_TRUE(chain->IsValid());
+    ASSERT_TRUE(taskD->Requires(chain.get()));
+
+    scheduler->Enqueue(taskD);
+    scheduler->Enqueue(chain);
+
+    taskA->Wait();
+    ASSERT_EQ(taskA->GetState(), TaskState::SUCCESS);
+    Console(std::cout) << "taskA complete\n";
+
+    taskB->Wait();
+    ASSERT_EQ(taskB->GetState(), TaskState::SUCCESS);
+    Console(std::cout) << "taskB complete\n";
+
+    taskC->Wait();
+    ASSERT_EQ(taskC->GetState(), TaskState::SUCCESS);
+    Console(std::cout) << "taskC complete\n";
+
+    chain->Wait();
+    ASSERT_EQ(chain->GetState(), TaskState::SUCCESS);
+    Console(std::cout) << "chain complete\n";
+
+    taskD->Wait();
+    ASSERT_EQ(taskD->GetState(), TaskState::SUCCESS);
+    Console(std::cout) << "taskD complete\n";
+
+    scheduler->Shutdown(true);
+    ASSERT_FALSE(scheduler->RunOnce());
+}
